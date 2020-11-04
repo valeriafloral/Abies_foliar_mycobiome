6cfb5c8e4c57609cbcda736a37c462ae1af755a9
valeriafloral <valeriaflores@ciencias.unam.mx>
2020-11-03 20:23:07 -0600

    update vector contamination script

--- parkinsonlabtutorial/bin/shell_scripts/2_vector.sh
+++ parkinsonlabtutorial/bin/shell_scripts/2_vector.sh
@@ -2,10 +2,11 @@
 #Valeria Flores
 
 #Perform alignments for reads with BWA and filter out any reads that align to our database with Samtools
+bwa mem ../../metadata/index/vector_contamination/UniVec_Core ../../data/trimmed/mouse1_unique.fastq > ../../data/trimmed/mouse1_univec_bwa.sam
 
-bwa mem ../../data/UniVec_Core ../../data/trimmed/mouse1_unique.fastq > ../../data/trimmed/mouse1_univec_bwa.sam
-
-#Transform .sam to .bam using samtools
-
+#Convert .sam to .bam using samtools
 samtools view -bS ../../data/trimmed/mouse1_univec_bwa.sam > ../../data/trimmed/mouse1_univec_bwa.bam
- 
+
+#Generate fastq outputs for all reads that mapped to the vector contaminant database (-F 4) and all reads that did not map to the vector contaminant database (-f 4)
+samtools fastq -n -F 4 -0 ../../data/trimmed/mouse1_univec_bwa_contaminats.fastq ../../data/trimmed/mouse1_univec_bwa.bam
+samtools fastq -n -f 4 -0 ../../data/trimmed/mouse1_univec_bwa.fastq ../../data/trimmed/mouse1_univec_bwa.bam
