445a00838d0df16078df66ee46031b481a02e2bf
valeriafloral <valeriaflores@ciencias.unam.mx>
2020-11-10 18:58:07 -0600

    update scripts

--- parkinsonlabtutorial/bin/shell_scripts/5_rereplication.sh
+++ parkinsonlabtutorial/bin/shell_scripts/5_rereplication.sh
@@ -3,3 +3,7 @@
 
 #Replace the previously removed replicate reads back into our data set
 python ../python_scripts/3_Reduplicate.py ../../data/trimmed/mouse1_qual.fastq ../../data/qual/mouse1_unique_mRNA.fastq ../../data/trimmed/mouse1_unique.fastq.clstr ../../data/qual/mouse1_mRNA.fastq
+
+
+#Check the quality
+fastqc ../../data/qual/mouse1_mRNA.fastq --outdir=../../metadata/fastqc
