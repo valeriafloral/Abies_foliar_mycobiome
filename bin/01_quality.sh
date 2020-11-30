85b90e4724a85d3286c367dcb0cbea77d8ac323b
valeriafloral <valeriaflores@ciencias.unam.mx>
2020-11-29 19:46:01 -0600

    update quality script

--- bin/01_quality.sh
+++ bin/01_quality.sh
@@ -5,15 +5,15 @@
 
 #Raw data fastQC analysis
 for f in ../../data/raw/*.fastq.gz;
-do fastqc --outdir=../metadata/fastqc;
+do fastqc --outdir=../metadata/reports/fastqc;
 done
 
-#Trimming with Trimmomatic-0.38
+#Trimming with Trimmomatic-0.39
 for f in $(ls *fastq.gz | sed 's/_[1-2].fastq.gz//' | sort -u);
-do trimmomatic PE -threads 8 -basein ${f}_1.fastq.gz -baseout ../data/trimmed/${f}.fastq.gz ILLUMINACLIP:/../metadata/adapters/TruSeq3-PE-2.fa:2:30:10 SLIDINGWINDOW:10:28 LEADING:28 TRAILING:28 MINLEN:50 HEADCROP:13;
+do trimmomatic PE -threads 8 -basein ../data/raw/${f}_1.fastq.gz -baseout ../data/filter/outputs/${f}.fastq.gz ILLUMINACLIP:/../data/filter/adapters/TruSeq3-PE-2.fa:2:30:10 SLIDINGWINDOW:10:28 LEADING:28 TRAILING:28 MINLEN:50 HEADCROP:13;
 done
 
 #Trimmed data quality with fastQC
-for f in ../../data/trimmed/*.fastq.gz;
+for f in ../../data/filter/outputs/*.fastq.gz;
 do fastqc --outdir=../metadata/fastqc;
 done
