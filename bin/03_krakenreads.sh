593304aa73e7b5334a252526931071c62f97ca7e
valeriafloral <valeriaflores@ciencias.unam.mx>
2021-03-28 22:08:16 -0600

    update kraken script

--- bin/03_krakenreads.sh
+++ bin/03_krakenreads.sh
@@ -10,18 +10,5 @@
 #Use concatenated files
 #Make the classification
 for i in tolerant damaged;
-do krakenuniq --db ../../programas/krakenuniq_db --fastq-input ../data/filter/outputs/${i}test.fastq --threads 12 --report-file ../data/reports/kraken${i}test.tsv;
-done
-
-###Work in progress
-
-#Variable with krona taxonomy folder
-taxonomy="/LUSTRE/Genetica/valeria/miniconda3/envs/krona/opt/krona/taxonomy"
-
-#Build the taxonomy
-ktUpdateTaxonomy.sh /LUSTRE/Genetica/valeria/miniconda3/envs/krona/opt/krona/taxonomy
-
-#Run krona
-for i in tolerant damaged;
-do ktImportTaxonomy ../data/reports/kraken/kraken${i}test.tsv -t 7 -s 6 -o ../data/reports/kraken/kraken${i}.html ;
+do krakenuniq --db ../../programas/krakenuniq_db --fastq-input ../data/filter/bulk/${i}.fastq --threads 12 --report-file ../data/reports/kraken${i}.tsv;
 done
