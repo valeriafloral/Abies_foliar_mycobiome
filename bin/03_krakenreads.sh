7ecc8741f4a056468864edceeebf9d1ded3662ed
valeriafloral <valeriaflores@ciencias.unam.mx>
2021-04-23 16:16:58 -0500

    add exact

--- bin/03_krakenreads.sh
+++ bin/03_krakenreads.sh
@@ -9,5 +9,5 @@
 
 #Assign taxonomy to reads using Krakenuniq and generate outputs analyse diversity and abundance
 for f in `ls ../data/taxonomy | grep ".fastq" | sed "s/.fastq//" | uniq`;
-do krakenuniq --db ../../programas/krakenuniq_db --fastq-input ../data/taxonomy/${f}.fastq --threads 12 --hll-precision 18 --report-file ../data/reports/kraken/reads/${f}_kraken_reads.report --output ../data/reports/kraken/reads/${f}_kraken_reads.kraken;
+do krakenuniq --db ../../programas/krakenuniq_db --fastq-input ../data/taxonomy/${f}.fastq --threads 12 --hll-precision 18 --exact --report-file ../data/reports/kraken/reads/${f}_kraken_reads.report --output ../data/reports/kraken/reads/${f}_kraken_reads.kraken;
 done
