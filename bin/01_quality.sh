e7654022d06b78905eeb9ff860cfcffc79966484
valeriafloral <valeriaflores@ciencias.unam.mx>
2021-04-27 16:23:07 -0500

    change output directory name

--- bin/01_quality.sh
+++ bin/01_quality.sh
@@ -23,19 +23,19 @@ multiqc ../data/reports/fastqc/raw -o ../data/reports/fastqc/raw
 for f in `ls ../data/RAW/ | grep '^D.*z$' | sed "s/_L007_R1_001.fastq.gz//"| sed "s/_L007_R2_001.fastq.gz//" | uniq`;
 do trimmomatic PE -threads 4 -phred33 \
 ../data/RAW/${f}_L007_R1_001.fastq.gz ../data/RAW/${f}_L007_R2_001.fastq.gz \
-../data/filter/outputs/${f}_L007_R1_001_paired.fq.gz ../data/filter/outputs/${f}_L007_R1_001_unpaired.fq.gz \
-../data/filter/outputs/${f}_L007_R2_001_paired.fq.gz ../data/filter/outputs/${f}_L007_R2_001_unpaired.fq.gz \
+../data/filter/trimmed/${f}_L007_R1_001_paired.fq.gz ../data/filter/trimmed/${f}_L007_R1_001_unpaired.fq.gz \
+../data/filter/trimmed/${f}_L007_R2_001_paired.fq.gz ../data/filter/trimmed/${f}_L007_R2_001_unpaired.fq.gz \
 ILLUMINACLIP:../data/filter/adapters/TruSeq3-PE-2.fa:2:30:10 SLIDINGWINDOW:10:28 LEADING:28 TRAILING:28 MINLEN:50 HEADCROP:13;
 done > ../data/reports/Trimmomatic.txt
 
 ##3: Quality trimmed data analysis
 
 #Trimmed data fastQC analysis
-for f in `ls ../data/filter/bwatest/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
-do fastqc ${f}_L007_R1_001_paired.fq.gz --outdir=../data/reports/trimmed/R1_paired;
-fastqc ${f}_L007_R2_001_paired.fq.gz --outdir=../data/reports/trimmed/R1_paired;
-fastqc ${f}_L007_R1_001_unpaired.fq.gz --outdir=../data/reports/trimmed/R1_unpaired;
-fastqc ${f}_L007_R2_001_unpaired.fq.gz --outdir=../data/reports/trimmed/R2_unpaired;
+for f in `ls ../data/filter/trimmed | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
+do fastqc ../data/filter/trimmed/${f}_L007_R1_001_paired.fq.gz --outdir=../data/reports/trimmed/R1_paired;
+fastqc ../data/filter/trimmed/${f}_L007_R2_001_paired.fq.gz --outdir=../data/reports/trimmed/R1_paired;
+fastqc ../data/filter/trimmed/${f}_L007_R1_001_unpaired.fq.gz --outdir=../data/reports/trimmed/R1_unpaired;
+fastqc ../data/filter/trimmed/${f}_L007_R2_001_unpaired.fq.gz --outdir=../data/reports/trimmed/R2_unpaired;
 done
 
 
