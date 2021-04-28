8c836237ba85eee004e69a4a7a7030268256dfdb
valeriafloral <valeriaflores@ciencias.unam.mx>
2021-04-28 00:27:14 -0500

    change extract unmapped reads and save fastq files

--- bin/02_removehost.sh
+++ bin/02_removehost.sh
@@ -4,40 +4,50 @@
 #SBATCH -n 6
 
 # Valeria Flores
-#26/01/2021
-#Remove Abies balsamea reads with BWA
+#Last update 27/04/2021
+#Remove Abies reads with BWA
 
-#Make index
+#Make host reference index
 bwa index -a bwtsw ../data/filter/reference/GGJG01.1.fasta
 samtools faidx ../data/filter/reference/GGJG01.1.fasta
 makeblastdb -in ../data/filter/reference/GGJG01.1.fasta -dbtype nucl
 
-#Perform alignments for paired and unpaired reads with BWA
-for f in `ls ../data/filter/outputs/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
-do bwa mem -t 4 ../data/filter/reference/GGJG01.1.fasta ../data/filter/outputs/${f}_L007_R1_001_paired.fq.gz ../data/filter/outputs/${f}_L007_R2_001_paired.fq.gz > ../data/filter/outputs/${f}_paired.sam;
-bwa mem -aM -t 4 ../data/filter/reference/GGJG01.1.fasta ../data/filter/outputs/${f}_L007_R1_001_unpaired.fq.gz > ../data/filter/outputs/${f}_R1_unpaired.sam;
-bwa mem -aM -t 4 ../data/filter/reference/GGJG01.1.fasta ../data/filter/outputs/${f}_L007_R2_001_unpaired.fq.gz > ../data/filter/outputs/${f}_R2_unpaired.sam;
+#Map reads against host reference
+for f in `ls ../data/filter/trimmed/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
+do bwa mem -t 4 ../data/filter/reference/GGJG01.1.fasta ../data/filter/trimmed/${f}_L007_R1_001_paired.fq.gz ../data/filter/trimm/${f}_L007_R2_001_paired.fq.gz > ../data/filter/nonhost/${f}_paired.sam;
+bwa mem -aM -t 4 ../data/filter/reference/GGJG01.1.fasta ../data/filter/trimmed/${f}_L007_R1_001_unpaired.fq.gz > ../data/filter/nonhost/${f}_R1_unpaired.sam;
+bwa mem -aM -t 4 ../data/filter/reference/GGJG01.1.fasta ../data/filter/trimmed/${f}_L007_R2_001_unpaired.fq.gz > ../data/filter/nonhost/${f}_R2_unpaired.sam;
 done
-
 
 #Convert .sam to .bam using samtools
-for f in `ls ../data/filter/outputs/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
-do samtools view -bS ../data/filter/outputs/${f}_paired.sam > ../data/filter/outputs/${f}_paired.bam;
-samtools view -bS ../data/filter/outputs/${f}_R1_unpaired.sam > ../data/filter/outputs/${f}_R1_unpaired.bam;
-samtools view -bS ../data/filter/outputs/${f}_R2_unpaired.sam > ../data/filter/outputs/${f}_R2_unpaired.bam;
+for f in `ls ../data/filter/trimmed/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
+do samtools view -bS ../data/filter/trimmed/${f}_paired.sam > ../data/filter/nonhost/${f}_paired.bam;
+samtools view -bS ../data/filter/trimmed/${f}_R1_unpaired.sam > ../data/filter/nonhost/${f}_R1_unpaired.bam;
+samtools view -bS ../data/filter/trimmed/${f}_R2_unpaired.sam > ../data/filter/nonhost/${f}_R2_unpaired.bam;
 done
 
-#Generate fastq outputs for all reads that did not map to the host reference genome (-f 4)
-#This is the datatset that is going to be used
-for f in `ls ../data/filter/outputs/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
-do samtools fastq -n -f 4 -1 ../data/filter/outputs/${f}_R1_paired_bulk.fastq -2 ../data/filter/outputs/${f}_R2_paired_bulk.fastq ../data/filter/outputs/${f}_paired.bam;
-samtools fastq -n -f 4 -0 ../data/filter/outputs/${f}_R1_unpaired_bulk.fastq ../data/filter/outputs/${f}_R1_unpaired.bam;
-samtools fastq -n -f 4 -0 ../data/filter/outputs/${f}_R2_unpaired_bulk.fastq ../data/filter/outputs/${f}_R2_unpaired.bam;
+#Extract unmapped reads (non-host)
+for f in `ls ../data/filter/trimmed/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
+do samtools view -b -f 12 -F 256 ../data/filter/nonhost/${f}_paired.bam > ../data/filter/nonhost/${f}_p_um.bam;
+samtools view -b -f 4 -F 256 ../data/filter/nonhost/${f}_R1_unpaired.bam > ../data/filter/nonhost/${f}_R1_up_um.bam;
+samtools view -b -f 4 -F 256 ../data/filter/nonhost/${f}_R2_unpaired.bam > ../data/filter/nonhost/${f}_R2_up_um.bam;
+done
+
+#Sort bam file to organize paired reads and convert bam paired files into fastq
+for f in `ls ../data/filter/trimmed/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
+do samtools sort -n ../data/filter/nonhost/${f}_p_um.bam -o ../data/filter/nonhost/${f}_p_um_sorted.bam;
+samtools bam2fq  ../data/filter/nonhost/${f}_p_um_sorted.bam > ../data/filter/nonhost/${f}_p_filtered.fastq;
+done
+
+#Convert bam unpaired files into fastq
+for f in `ls ../data/filter/trimmed/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
+do samtools bam2fq ../data/filter/nonhost/${f}_R1_up_um.bam > ../data/filter/nonhost/${f}_R1_UP_filtered.fastq;
+samtools bam2fq ../data/filter/nonhost/${f}_R2_up_um.bam > ../data/filter/nonhost/${f}_R2_UP_filtered.fastq;
 done
 
 #Statistics
-for f in `ls ../data/filter/outputs/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
-do samtools stats ../data/filter/outputs/${f}_paired.bam > ../data/reports/mapping/${f}_hostmap_paired.txt;
-samtools stats ../data/filter/outputs/${f}_R1_unpaired.bam > ../data/reports/mapping/${f}_hostmap_R1_unpaired.txt;
-samtools stats ../data/filter/outputs/${f}_R2_unpaired.bam > ../data/reports/mapping/${f}_hostmap_R2_unpaired.txt;
+for f in `ls ../data/filter/trimmed/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
+do samtools stats ../data/filter/nonhost/${f}_paired.bam > ../data/reports/mapping/${f}_hostmap_paired.txt;
+samtools stats ../data/filter/nonhost/${f}_R1_unpaired.bam > ../data/reports/mapping/${f}_hostmap_R1_unpaired.txt;
+samtools stats ../data/filter/nonhost/${f}_R2_unpaired.bam > ../data/reports/mapping/${f}_hostmap_R2_unpaired.txt;
 done
