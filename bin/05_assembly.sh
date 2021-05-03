162d41e8bc82f814fa25525c78049c7671b14c00
valeriafloral <valeriaflores@ciencias.unam.mx>
2021-05-03 14:49:21 -0500

    complete assembly script

--- bin/05_assembly.sh
+++ bin/05_assembly.sh
@@ -4,15 +4,15 @@
 #SBATCH -n 4
 
 # Valeria Flores
-#23/04/2021
+#03/04/2021
 
 #Before running this script make sure to create a folder for each sample
 #Assembly with metaSPADEs
-for f in `ls ../data/filter/outputs/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
-do spades.py --rna --12 ../data/filter/outputs/${f}_paired_bulk.fastq --s1 ../data/filter/outputs/${f}_R1_unpaired_bulk.fastq --s2 ../data/filter/outputs/${f}_R2_unpaired_bulk.fastq -t 4 -k 21,33,55,77,99,111,127 -o ../data/reports/assembly/${f}_assembly;
+for f in `ls ../data/filter/nonhost/ | grep ".fastq" | sed "s/_p_filtered.fastq//" | sed "s/_R1_filtered.fastq//" | sed "s/_R2_filtered.fastq//"| sed "s/_cat.fastq//" | uniq`;
+do spades.py --rna --12 ../data/filter/nonhost/${f}_p_filtered.fastq --s1 ../data/filter/nonhost/${f}_R1_filtered.fastq --s2 ../data/filter/nonhost/${f}_R2_filtered.fastq -t 4 -k 21,33,55,77,99,111,127 -o ../data/reports/assembly/${f}_assembly
 done
 
 #Count contigs
-for f in `ls ../data/filter/outputs/ | grep ".fq.gz" | sed "s/_L007_R1_001_paired.fq.gz//"| sed "s/_L007_R2_001_paired.fq.gz//" | sed "s/_L007_R1_001_unpaired.fq.gz//"| sed "s/_L007_R2_001_unpaired.fq.gz//"| uniq`;
+for f in `ls ../data/filter/nonhost/ | grep ".fastq" | sed "s/_p_filtered.fastq//" | sed "s/_R1_filtered.fastq//" | sed "s/_R2_filtered.fastq//"| sed "s/_cat.fastq//" | uniq`;
 do grep -c '>' ../data/reports/assembly/${f}_assembly > ../data/reports/assembly/contigs.txt;
 done
