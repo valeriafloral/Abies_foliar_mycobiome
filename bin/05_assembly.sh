064cab7180cdaf2946ec9128d39057b74645d4de
valeriafloral <valeriaflores@ciencias.unam.mx>
2021-05-12 13:43:16 -0500

    change otput route

--- bin/05_assembly.sh
+++ bin/05_assembly.sh
@@ -9,10 +9,5 @@
 #Before running this script make sure to create a folder for each sample
 #Assembly with metaSPADEs
 for f in `ls ../data/filter/nonhost/ | grep ".fastq" | sed "s/_p_filtered.fastq//" | sed "s/_R1_filtered.fastq//" | sed "s/_R2_filtered.fastq//"| sed "s/_cat.fastq//" | uniq`;
-do spades.py --rna --12 ../data/filter/nonhost/${f}_p_filtered.fastq --s1 ../data/filter/nonhost/${f}_R1_filtered.fastq --s2 ../data/filter/nonhost/${f}_R2_filtered.fastq -t 4 -k 21,33,55,77,99,111,127 -o ../data/reports/assembly/${f}_assembly
-done
-
-#Count contigs
-for f in `ls ../data/filter/nonhost/ | grep ".fastq" | sed "s/_p_filtered.fastq//" | sed "s/_R1_filtered.fastq//" | sed "s/_R2_filtered.fastq//"| sed "s/_cat.fastq//" | uniq`;
-do grep -c '>' ../data/reports/assembly/${f}_assembly > ../data/reports/assembly/contigs.txt;
+do spades.py --rna --12 ../data/filter/nonhost/${f}_p_filtered.fastq --s1 ../data/filter/nonhost/${f}_R1_filtered.fastq --s2 ../data/filter/nonhost/${f}_R2_filtered.fastq -t 4 -k 21,33,55,77,99,111,127 -o ../data/assembly/${f}_assembly;
 done
