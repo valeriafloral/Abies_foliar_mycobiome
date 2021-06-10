155f7df9fd5822ea5670b1c967dcf7b4ae52c2bc
valeriafloral <valeriaflores@ciencias.unam.mx>
2021-06-09 20:33:27 -0500

    change krakenuniq for kraken2

--- bin/03_krakenreads.sh
+++ bin/03_krakenreads.sh
@@ -5,12 +5,12 @@
 
 # Valeria Flores
 #23/04/2021
-#Assign taxonomy to reads using Krakenuniq
+#Assign taxonomy to reads using Kraken2
 
 #Assign taxonomy to reads using Krakenuniq for paired, r1. r2 and concatenated files
 for f in `ls ../data/filter/nonhost/ | grep ".fastq" | sed "s/_p_filtered.fastq//" | sed "s/_R1_filtered.fastq//" | sed "s/_R2_filtered.fastq//"| sed "s/_cat.fastq//" | uniq`;
-do krakenuniq --db ../../programas/krakenuniq_db --fastq-input ../data/filter/nonhost/${f}_p_filtered.fastq --threads 12 --hll-precision 18 --exact --report-file ../data/reports/kraken/${f}_k_reads_paired.report --output ../data/reports/kraken/${f}_k_reads_paired.kraken --only-classified-output;
-krakenuniq --db ../../programas/krakenuniq_db --fastq-input ../data/filter/nonhost/${f}_R1_filtered.fastq --threads 12 --hll-precision 18 --exact --report-file ../data/reports/kraken/${f}_k_reads_R1.report --output ../data/reports/kraken/${f}_k_reads_R1.kraken --only-classified-output;
-krakenuniq --db ../../programas/krakenuniq_db --fastq-input ../data/filter/nonhost/${f}_R2_filtered.fastq --threads 12 --hll-precision 18 --exact --report-file ../data/reports/kraken/${f}_k_reads_R2.report --output ../data/reports/kraken/${f}_k_reads_R2.kraken --only-classified-output;
-krakenuniq --db ../../programas/krakenuniq_db --fastq-input ../data/filter/nonhost/${f}_cat.fastq --threads 12 --hll-precision 18 --exact --report-file ../data/reports/kraken/${f}_k_reads_cat.report --output ../data/reports/kraken/${f}_k_reads_cat.kraken --only-classified-output;
+do kraken2 --db fungi --fastq-input ../data/filter/nonhost/${f}_p_filtered.fastq --threads 12 --report ../data/reports/kraken2/${f}_k2_reads_paired.report --use-mpa-style --output ../data/reports/kraken2/${f}_k2_reads_paired.kraken;
+kraken2 --db fungi --fastq-input ../data/filter/nonhost/${f}_R1_filtered.fastq --threads 12 --report ../data/reports/kraken2/${f}_k2_reads_R1.report --use-mpa-style --output ../data/reports/kraken2/${f}_k2_reads_R1.kraken;
+kraken2 --db fungi --fastq-input ../data/filter/nonhost/${f}_R2_filtered.fastq --threads 12 --report ../data/reports/kraken2/${f}_k2_reads_R2.report --use-mpa-style --output ../data/reports/kraken2/${f}_k2_reads_R2.kraken;
+kraken2 --db fungi --fastq-input ../data/filter/nonhost/${f}_cat.fastq --threads 12 --report ../data/reports/kraken2/${f}_k2_reads_cat.report --use-mpa-style --output ../data/reports/kraken2/${f}_k2_reads_cat.kraken;
 done
