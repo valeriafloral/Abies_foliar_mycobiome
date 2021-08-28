c46151f92c7e8e0fad75275e2b2a34437efa29a2
valeriafloral <valeriaflores@ciencias.unam.mx>
2021-08-28 16:01:11 -0500

    edited

--- bin/07_krakencontigs.sh
+++ bin/07_krakencontigs.sh
@@ -4,10 +4,11 @@
 #SBATCH -n 6
 
 # Valeria Flores
-#11/05/2021
-#Assign taxonomy to SPAdes assembled contigs using Krakenuniq
+#11/06/2021
+#Assign taxonomy to contigs using Kraken2
 
 #Assign taxonomy to reads using Krakenuniq for paired, r1. r2 and concatenated files
 for f in `ls ../data/filter/nonhost/ | grep ".fastq" | sed "s/_p_filtered.fastq//" | sed "s/_R1_filtered.fastq//" | sed "s/_R2_filtered.fastq//"| sed "s/_cat.fastq//" | uniq`;
-do krakenuniq --db ../../programas/krakenuniq_db --fasta-input ../data/assembly/${f}_assembly/transcripts.fasta --threads 12 --hll-precision 18 --exact --report-file ../data/reports/kraken/${f}_k_contigs.report --output ../data/reports/kraken/${f}_k_contigs.kraken --only-classified-output;
+do kraken2 --db ../../programas/kraken2_db/fungi ../data/assembly/${f}_assembly/transcripts.fasta --threads 12 --report ../data/reports/kraken2/${f}_k2_contigs.report --output ../data/reports/kraken2/${f}_k2_contigs.kraken;
 done
+
