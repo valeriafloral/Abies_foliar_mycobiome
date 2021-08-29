cfc6255ba30f887ba255fc7336e7240252bdd127
valeriafloral <valeriaflores@ciencias.unam.mx>
2021-08-28 21:13:08 -0500

    edited kraken contigs

--- bin/07_krakencontigs.sh
+++ bin/07_krakencontigs.sh
@@ -11,3 +11,18 @@
 for f in `ls ../data/filter/nonhost/ | grep ".fastq" | sed "s/_p_filtered.fastq//" | sed "s/_R1_filtered.fastq//" | sed "s/_R2_filtered.fastq//"| sed "s/_cat.fastq//" | uniq`;
 do krakenuniq --db ../../programas/krakenuniq_db --fasta-input ../data/assembly/${f}_assembly/transcripts.fasta --threads 12 --hll-precision 18 --exact --report-file ../data/reports/kraken/${f}_k_contigs.report --output ../data/reports/kraken/${f}_k_contigs.kraken --only-classified-output;
 done
+
+#Estimate abundance with bracken
+for f in `ls ../data/filter/nonhost/ | grep ".fastq" | sed "s/_p_filtered.fastq//" | sed "s/_R1_filtered.fastq//" | sed "s/_R2_filtered.fastq//"| sed "s/_cat.fastq//" | uniq`;
+do bracken -d ../../programas/kraken2_db/fungi -i ../data/reports/kraken2/${f}_k2_contigs.report -o ../data/reports/kraken2/${f}_k2_contigs.bracken -r 100 -l S -t 1;
+done
+
+#Make a mpa report style to easier export it to R
+for f in `ls ../data/filter/nonhost/ | grep ".fastq" | sed "s/_p_filtered.fastq//" | sed "s/_R1_filtered.fastq//" | sed "s/_R2_filtered.fastq//"| sed "s/_cat.fastq//" | uniq`;
+do kreport2mpa.py -r ../data/reports/kraken2/${f}_k2_contigs_bracken_species.report -o ../data/reports/kraken2/${f}_k2_contigs_bracken.txt --no-intermediate-ranks --read_count --display-header;
+done
+
+#Combine
+combine_mpa.py -i ../data/reports/kraken2/*_k2_contigs_bracken.txt -o ../data/reports/kraken2/combined_sp_contigs_bracken.txt
+
+grep -E "(s__)|(#Classification)" ../data/reports/kraken2/combined_sp_contigs_bracken.txt > ../data/reports/kraken2/contigs_kraken2.txt
