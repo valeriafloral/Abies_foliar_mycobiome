bf0b21f89f2344a8bf11ab61fca0e5133b6bea0d
valeriafloral <valeriaflores@ciencias.unam.mx>
2021-08-28 23:32:38 -0500

    fix the errors

--- bin/07_krakencontigs.sh
+++ bin/07_krakencontigs.sh
@@ -4,12 +4,12 @@
 #SBATCH -n 6
 
 # Valeria Flores
-#11/05/2021
+#28/08/2021
 #Assign taxonomy to SPAdes assembled contigs using Krakenuniq
 
 #Assign taxonomy to reads using Krakenuniq for paired, r1. r2 and concatenated files
 for f in `ls ../data/filter/nonhost/ | grep ".fastq" | sed "s/_p_filtered.fastq//" | sed "s/_R1_filtered.fastq//" | sed "s/_R2_filtered.fastq//"| sed "s/_cat.fastq//" | uniq`;
-do krakenuniq --db ../../programas/krakenuniq_db --fasta-input ../data/assembly/${f}_assembly/transcripts.fasta --threads 12 --hll-precision 18 --exact --report-file ../data/reports/kraken/${f}_k_contigs.report --output ../data/reports/kraken/${f}_k_contigs.kraken --only-classified-output;
+do kraken2 --db fungi --fasta-input ../data/assembly/${f}_assembly/transcripts.fasta --threads 12 --report ../data/reports/kraken2/${f}_k2_contigs.report --output ../data/reports/kraken2/${f}_k2_contigs.kraken;
 done
 
 #Estimate abundance with bracken
@@ -25,4 +25,5 @@ done
 #Combine
 combine_mpa.py -i ../data/reports/kraken2/*_k2_contigs_bracken.txt -o ../data/reports/kraken2/combined_sp_contigs_bracken.txt
 
+#Keep only clssified at species level
 grep -E "(s__)|(#Classification)" ../data/reports/kraken2/combined_sp_contigs_bracken.txt > ../data/reports/kraken2/contigs_kraken2.txt
